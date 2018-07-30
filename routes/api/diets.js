ab674468a1030d13746c01b42f5d58a00ba69435
Mateusz Skibicki <skibickiengland@gmail.com>
2018-07-30 23:45:22 +0100

    Testing profile

--- routes/api/diets.js
+++ routes/api/diets.js
@@ -225,31 +225,35 @@ router.post(
 			dietFields.user = req.user._id;
 			dietTags.length > 0 ? dietFields.tags = dietTags : '';
 			new Diet(dietFields).save().then(diet => {
-				console.log(diet);
-				// Diet.find().sort({date: 1}).populate('user')
-				// 	.then(dietsWithNew => {
-				// 		User.findById(req.user._id).then(user => {
-				// 			let userWithDiet = user.diets.unshift(diet._id);
-				// 			user.save(userWithDiet)
-				// 				.then((user)=> console.log('New data'))
-				// 				.catch(e => console.log(e));	
-				// 		});
-				// 		let allDiets = []; // empty array
-				// 		dietsWithNew.map(diet => { // map through array
-				// 			let dietWithUser = {
-				// 				...diet._doc,
-				// 				user: {
-				// 					_id: diet.user._id,
-				// 					avatar: diet.user.avatar,
-				// 					name: diet.user.name,
-				// 					nickname: diet.user.nickname
-				// 				}
-				// 			};
-				// 			allDiets.unshift(dietWithUser);
-				// 		});
-				// 		return res.json(allDiets);
-				// 	})
-				// 	.catch(e => console.log(e));
+				console.log('1');
+				Diet.find().sort({date: 1}).populate('user')
+					.then(dietsWithNew => {
+						console.log('2');
+						User.findById(req.user._id).then(user => {
+							console.log('3');
+							let userWithDiet = user.diets.unshift(diet._id);
+							user.save(userWithDiet)
+								.then((user)=> console.log('New data'))
+								.catch(e => console.log(e));	
+						});
+						let allDiets = []; // empty array
+						dietsWithNew.map(diet => { // map through array
+							console.log('4');
+							let dietWithUser = {
+								...diet._doc,
+								user: {
+									_id: diet.user._id,
+									avatar: diet.user.avatar,
+									name: diet.user.name,
+									nickname: diet.user.nickname
+								}
+							};
+							allDiets.unshift(dietWithUser);
+						});
+						console.log('5');
+						return res.json(allDiets);
+					})
+					.catch(e => console.log(e));
 			});
 		}
 	});
